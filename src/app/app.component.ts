8a8905b24dbeea74238416c94493234a89c6cce3
Shuva Biswas <shuvabiswas12@gmail.com>
2022-04-20 13:52:42 +0600

    added crud feature for firebase using angularFire

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,20 @@
 import { Component, OnDestroy } from '@angular/core';
-import { collection, collectionData, Firestore } from '@angular/fire/firestore';
+import {
+  collection,
+  collectionData,
+  collectionSnapshots,
+  Firestore,
+} from '@angular/fire/firestore';
+
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+
+// import {
+//   AngularFirestore,
+//   AngularFirestoreDocument,
+// } from '@angular/fire/compat/firestore';
+
 import { map, Observable, Subscription } from 'rxjs';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,17 +23,18 @@ import { map, Observable, Subscription } from 'rxjs';
 })
 export class AppComponent implements OnDestroy {
   title = 'firebase-demo';
+  inputText: string = ' ';
   // courses = [];
   // subscription: Subscription;
 
   // another way to do async...
-  courses$; // here $ sign means this is observable, that is why we do not need to write more code for getting subscription
+  courses$: Observable<any[]>; // here $ sign means this is observable, that is why we do not need to write more code for getting subscription
   // after this we need to add a async pipe in html file after this courses$ variable.
   // this async pipe automatically update the component with the new data when any changes detected by.
   // and automatically unsubscribe the subscription when destroy the component.
   // that async pipe unwrap the observable in the html file.
 
-  constructor(fireStore: Firestore) {
+  constructor(private fireStore: AngularFirestore) {
     // this code for when we will not use async pipe and courses$ observable.
 
     // this.subscription = collectionData(collection(fireStore, 'courses'))
@@ -39,7 +54,43 @@ export class AppComponent implements OnDestroy {
     //   });
 
     // this code line for when we use async pipe and courses$ observable
-    this.courses$ = collectionData(collection(fireStore, 'courses'));
+
+    this.courses$ = fireStore
+      .collection('courses', (ref) => ref.orderBy('createdAt', 'desc'))
+      .valueChanges({ idField: 'eventId' });
+
+    // this eventId is actual documents id which i pass later for update and delete that particular documents
+  }
+
+  saveData(data: HTMLInputElement) {
+    console.log(data.value);
+    this.fireStore
+      .collection('courses')
+      .add({ title: data.value, createdAt: new Date() });
+    data.value = '';
+  }
+
+  saveData_v2() {
+    this.fireStore
+      .collection('courses')
+      .add({ title: this.inputText.trim(), createdAt: new Date() });
+    this.inputText = '';
+  }
+
+  update(eventId: string) {
+    let courseDoc = this.fireStore.doc('courses/'.concat(eventId));
+    courseDoc.update({
+      title: 'Hey, champ, why are you crying? this is too much.',
+    });
+  }
+
+  delete(eventId: string) {
+    let courseDoc = this.fireStore.doc('courses/'.concat(eventId));
+    courseDoc.delete();
+  }
+
+  submit(form: NgForm) {
+    console.log(form.form.value);
   }
 
   ngOnDestroy(): void {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-import {} from '@angular/fire';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
-    provideFirestore(() => getFirestore()),
+    // provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    // provideFirestore(() => getFirestore()),
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
